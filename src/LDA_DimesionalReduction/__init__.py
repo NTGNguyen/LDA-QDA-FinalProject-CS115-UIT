3bbd6d645667830a51eddb02c3619f339073c6c8
NTGNguyen <23521049@gm.uit.edu.vn>
2024-12-05 17:54:11 +0700

    ♻️ refactor: restructure LDA modules and update StepByStepExplain class

---
+++ src/LDA_DimesionalReduction/__init__.py
@@ -0,0 +1,11 @@
+from LDA_with_Datasets import LDATwoBlobs,LDAIrisDataSetTwoComponentVisualize,LDAIrisDataSetOneComponentVisualize
+from LDA import LDA
+from Step_by_Step import StepByStepExplain
+
+__all__ = [
+    'LDATwoBlobs',
+    'LDAIrisDataSetTwoComponentVisualize',
+    'LDAIrisDataSetOneComponentVisualize',
+    'LDA',
+    'StepByStepExplain'
+]
